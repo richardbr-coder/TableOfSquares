318a0d29e561345bec53f9056aca3b3d47fda1f7
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:31:29 -0230

    Add files via upload

---
+++ TableOfSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableOfSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double counter;
+            const int maxValue = 20;
+            double square;
+
+            Console.WriteLine("Table Of Squares");
+            Console.WriteLine("________________");
+            Console.WriteLine("X     X-Square");
+            Console.WriteLine("----------------");
+
+            for (counter = 1; counter <= maxValue; counter++)
+            {
+                square = Math.Pow(counter, 2);
+                Console.WriteLine("{0}        {1}", counter, square);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
